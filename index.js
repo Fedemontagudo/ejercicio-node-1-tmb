bfde1e71e8368ad0bca97244818dc91640a8f7d8
Fede Montagud <fedemontagudo@gmail.com>
2021-03-31 23:58:04 +0200

    cambiada estructura general, mensaje se consolea en colores ok

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ program
 program.parse(process.argv);
 const options = program.opts();
 console.log(options);
+const { abrev, color } = options;
 
 /* fetch(`${process.env.URL}?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`)
     .then(resp => resp.json())
@@ -21,9 +22,21 @@ inquirer.prompt(preguntas)
     if (respuestas.tipo === "Bus") {
       console.log(chalk.yellow("No tenemos información disponible de las paradas. Por favor, diríjase a https://www.tmb.cat/ca/home"));
       process.exit(0);
-    } else {
-      (fetch(`${process.env.URL}?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`)
+    }
+    if (respuestas.lineasConsulta) {
+      fetch(`${process.env.URL}?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`)
         .then(resp => resp.json())
-        .then(lineas => console.log(lineas.features.find(linea => linea.properties.NOM_LINIA === respuestas.lineasConsulta))));
+        .then(lineas => {
+          const linea = lineas.features.find(linea => linea.properties.NOM_LINIA === respuestas.lineasConsulta);
+          if (linea === undefined) {
+            if (respuestas.informacionErrores) {
+              console.log(chalk.red.bold("ERROR, no existe la línea"));
+              process.exit(0);
+            } else {
+              process.exit(0);
+            }
+          }
+          console.log(chalk.hex(`${color === true ? color : `${linea.properties.COLOR_LINIA}`}`)(`Nom de la linia: ${linea.properties.NOM_LINIA} , descripció: ${linea.properties.DESC_LINIA}`));
+        });
     }
   });
